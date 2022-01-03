6b1d72b1705aca88188fc896ea318c468315ad9c
Todd Bryant <hello@toddaas.com>
2022-01-03 16:07:21 +1100

    Ensure Netfirewall is highly available and AZ fault Tolerant

--- lib/netfirewall-stack.ts
+++ lib/netfirewall-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import { AmazonLinuxImage, InstanceType } from '@aws-cdk/aws-ec2';
+import * as networkfirewall from '@aws-cdk/aws-networkfirewall';
 
 export class NetfirewallStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -16,6 +17,7 @@ class InspectionVpcStack extends cdk.NestedStack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.NestedStackProps) {
     super(scope, id, props);
 
+    // Create VPC and Subnets
     const inspectionVpc = new ec2.Vpc(this, 'InspectionVpcStack', {
       cidr: "10.0.0.0/16",
       maxAzs: this.availabilityZones.length,
@@ -27,20 +29,47 @@ class InspectionVpcStack extends cdk.NestedStack {
         },
         {
           cidrMask: 24,
-          name: 'Inspection',
+          name: 'Private',
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED
         },
         {
           cidrMask: 24,
-          name: 'Private',
+          name: 'Inspection',
           subnetType: ec2.SubnetType.PRIVATE_WITH_NAT
         }
       ]
     });
 
+    // Create Network Firewall
+    const netFirewallPolicy = new networkfirewall.CfnFirewallPolicy(this, 'netFirewallPolicy', {
+      firewallPolicy: {
+        statelessDefaultActions: ['aws:forward_to_sfe'],
+        statelessFragmentDefaultActions: ['aws:forward_to_sfe']
+      },
+      firewallPolicyName: 'netFirewallPolicy',
+    });
+
+    const inspectionSubnets = inspectionVpc.selectSubnets({
+      subnetType: ec2.SubnetType.PRIVATE_WITH_NAT
+    });
+
+    const inspectionSubnetMapping = inspectionSubnets.subnets.map(subnet => {
+      return {
+        subnetId: subnet.subnetId
+      }
+    });
+
+    const netFirewall = new networkfirewall.CfnFirewall(this, 'netFirewall', {
+      firewallName: 'netFirewall',
+      firewallPolicyArn: netFirewallPolicy.attrFirewallPolicyArn,
+      subnetMappings: inspectionSubnetMapping,
+      vpcId: inspectionVpc.vpcId,
+    });
+
+    // Create Instances in Public & Private Subnets
     new ec2.Instance(this, 'Instance', {
       vpc: inspectionVpc,
-      instanceType: new InstanceType('t2.micro'),
+      instanceType: new InstanceType('t2.micro'), 
       machineImage: new AmazonLinuxImage
     });
   }
@@ -56,3 +85,4 @@ class SpokeVpc extends cdk.NestedStack {
     });
   }
 }
+
