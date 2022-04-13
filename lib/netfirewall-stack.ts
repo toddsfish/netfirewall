a869e9267c9c41bc949a9b98212d863251a04666
Todd <hello@toddaas.com>
2022-04-13 16:15:40 +1000

    added comment to show strict order

--- lib/netfirewall-stack.ts
+++ lib/netfirewall-stack.ts
@@ -42,7 +42,7 @@ class InspectionVpcStack extends cdk.NestedStack {
       ]
     });
 
-    // Create Network Firewall
+    // Create Network Firewall Strict Order
     const netFirewallPolicy = new networkfirewall.CfnFirewallPolicy(this, 'netFirewallPolicy', {
       firewallPolicy: {
         statelessDefaultActions: ['aws:forward_to_sfe'],
